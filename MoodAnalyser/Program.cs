fe989b3c62f6060c75d8e92dd90582685952ad65
vaibhav sable <vaibhavsable52@gmail.com>
2022-06-03 07:06:06 -0700

    MoodAnalyser

--- MoodAnalyser/Program.cs
+++ MoodAnalyser/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class Mood
+    {
+        public String message;
+        public Mood(String message)
+        {
+
+            this.message = message;
+        }
+        // uc1
+
+        //public String AnalyseMood()
+        //{
+
+        //    if (this.message.Contains("sad"))
+        //    {
+        //        return "SAD";
+        //    }
+        //    else
+        //    {
+        //        return "HAPPY";
+        //    }
+        //}
+
+
+        // uc2
+        public String AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Contains("sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch
+            {
+                return "Happy";
+            }
+        }
+
+
+    }
+}
